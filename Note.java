1c291df3e243226ed897e4f19da4787ddd1e026c
Promethicon <Promethicon@gmail.com>
2017-03-29 15:55:09 -0400

    Add files via upload

--- Note.java
+++ Note.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Note
 {
    public String letter;
@@ -8,56 +10,82 @@ public class Note
    public String length;
    
    //[Letter, Length]
-   public Note(String s, String l){
+   public Note(String s, String l)
+   {
+      String regex = "\\d+/\\d+";
+      Pattern p = Pattern.compile(regex);
+      Matcher m = p.matcher(l);
+      
       parseLetter(s); //Run checks and set letter and isMajor/Natural/Sharp values
+      if(m.find())
+      {
       this.length = l;
+      }
+      else
+      {
+         throw new RuntimeException("Illegal note length format");
+      }
    }
    
    
    public void setLetter(char x)
    {
-      if(!isValidLetter(x)) throws new RuntimeException("Not a valid letter");
+      if(!isLetter(x)) throw new RuntimeException("Not a valid letter");
       letter = String.valueOf(x);
    }
    
-   public static boolean isLetter(String x){
+   public static boolean isLetter(char x)
+   {
       return ((x == 'a') || (x == 'A') || (x == 'b') || (x == 'B') || (x == 'c') || (x == 'C') || (x == 'd') 
          || (x == 'D') || (x == 'e') || (x == 'E') || (x == 'f') || (x == 'F') || (x == 'g') || (x == 'G'));
    }
    
-   public static boolean isMajor(char x){
+   public static boolean isMajor(char x)
+   {
       int i = (int)x;
       return (i >= 65 && i <= 71);
    }
    
  
-   private void parseLetter(String s){
+   private void parseLetter(String s)
+   {
       
-      if(s.length() > 1){
+      if(s.length() > 1)
+      {
          char tokens[] = s.toCharArray(s);
          setLetter(tokens[1]);
-         //check =^_
+         
+         //check to see if the note is an accidental "=^_"
          char accidental = tokens[0];
-         if(accidental == '_'){
+         
+         if(accidental == '_')
+         {
             isFlat = true;
             isNatural = false;
             isSharp = false;
-         } else if(accidental == '='){
+         } 
+         else if(accidental == '=')
+         {
             isFlat = false;
             isNatural = true;
             isSharp = false;
          
-         } else if(accidental == '^'){
+         } 
+         else if(accidental == '^')
+         {
             isFlat = false;
             isNatural = false;
             isSharp = true;
-         } else {
-            //throw error
-            throws new RuntimeException("Not a vaild accidental");
+         } 
+         else //throw error
+         {
+            throw new RuntimeException("Not a vaild accidental");
          }
          
          this.isMajor = isMajor(tokens[1]);
-      } else {
+      } 
+      else 
+      {
          setLetter(s);
          this.isMajor = isMajor(s.charAt(0));
       }
---
+++ Song.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public Class Song
+{
+   private String title;
+   private String key;
+   private String noteLength; //set the default length of the notes
+   public ArrayList<Measure> measures = new ArrayList<Measure>(); //this holds the measures that compose the song
+      
+   public Song()
+   {
+      title = "A";
+      key = "aMaj";
+      noteLength = "1/4";
+   }
+   public Song(String t, String k, String n)
+   {
+      String regEx = "\\d+/\\d+";
+      Pattern p = Pattern.compile(regex);
+      Matcher m = p.matcher(n);
+      
+      title = t;
+      key = k;
+      if(m.find())   //check to see that the note length is in the proper format
+      {
+         noteLength = n;
+      }
+      else
+      {
+         throw new RuntimeException("Illegal note length format");
+      }
+   }
+}
